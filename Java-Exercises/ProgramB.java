5258e633749fb14577d9671965620474f1f069e3
Gorka Garcia <garcia.gorka@uni.eus>
2023-10-11 10:57:05 +0200

    Make programB

--- Java-Exercises/ProgramB.java
+++ Java-Exercises/ProgramB.java
@@ -1,29 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
+import java.io.OutputStream;
+import java.io.Writer;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 
 public class ProgramB {
     public static void main(String[] args) {
-        String line;
         try {
+            // Create the procesBuilder to execute the command
+            ProcessBuilder processBuilder = new ProcessBuilder("java", "ProgramA");
+            Process processA = processBuilder.start();
 
-            Process hijo = new ProcessBuilder(".//ProgramA").start();
-            BufferedReader br = new BufferedReader(new InputStreamReader(hijo.getInputStream()));
-            PrintStream ps = new PrintStream(hijo.getOutputStream());
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            // Ctrl-D to get out the loop
-            while ((line = in.readLine()) != null) 
-            {
-                ps.println(line);
-                ps.flush();
-                if ((line = br.readLine()) != null) {
-                    System.out.println(line);
-                }
+            // Get the outputStream of program A to enter its input
+            OutputStream outputStreamA = processA.getOutputStream();
+            Writer writerA = new BufferedWriter(new OutputStreamWriter(outputStreamA));
+            
+            // Get the input of the user
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Write the input of programA: ");
+            String inputA = br.readLine();
+            writerA.write(inputA + "\n");
+            writerA.flush();
+            writerA.close();
+
+            // Read and show the output of program A
+            BufferedReader readerA = new BufferedReader(new InputStreamReader(processA.getInputStream()));
+            String lineA;
+            while ((lineA = readerA.readLine()) != null) {
+                System.out.println("ProgramA's output: " + lineA);
             }
-            System.out.println(ps);
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
+
+            // Wait to program A finishes
+            int exitCodeA = processA.waitFor();
+            System.out.println("programA executed with exit code " + exitCodeA);
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
