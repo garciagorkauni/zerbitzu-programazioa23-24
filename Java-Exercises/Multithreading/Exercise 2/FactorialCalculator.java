0a811290745a79f26194150a58cb652c84be23f4
gorka-cs <114485898+garciagorkauni@users.noreply.github.com>
2023-11-02 15:37:13 +0100

    Exercise2 made

---
+++ Java-Exercises/Multithreading/Exercise 2/FactorialCalculator.java
@@ -0,0 +1,24 @@
+class FactorialCalculator extends Thread {
+  // Define variables
+  private long start, end;
+  private SharedSum totalSum;
+
+  // Define the constructor of the thread
+  public FactorialCalculator(long start, long end, SharedSum totalSum) {
+      this.start = start;
+      this.end = end;
+      this.totalSum = totalSum;
+  }
+  
+  // Calculate the wanted sum
+  @Override
+  public void run() {
+    long partialSum = 0;
+
+    for (long i = start; i <= end; i++) {
+        partialSum += i;
+    }
+
+    totalSum.add(partialSum);
+  }
+}
---
+++ Java-Exercises/Multithreading/Exercise 2/SharedSum.java
@@ -0,0 +1,11 @@
+class SharedSum {
+    private long totalSum = 0;
+
+    public synchronized void add(long partialSum) {
+        totalSum += partialSum;
+    }
+
+    public long getTotalSum() {
+        return totalSum;
+    }
+}
